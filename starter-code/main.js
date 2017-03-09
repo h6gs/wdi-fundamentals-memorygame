e1ee823bd670159343b64ee7b755af790b57c742
Harry Stirling <harrystirling@sh.local>
2017-03-09 12:53:51 +0000

    final memory game code

--- starter-code/main.js
+++ starter-code/main.js
@@ -9,16 +9,46 @@ if (cardOne === cardTwo || cardThree === cardFour) {
   alert('Sorry, try again.');
 }
 */
+
 var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+
 var board = document.getElementById('game-board');
 
-function createCards() {
-  for (var i=0; i < cards.length; i++) {
-    var cardElement = document.createElement('div')
+function createBoard() {
+  for (var i=0; i<cards.length; i++) {
+    var cardElement = document.createElement('div');
     cardElement.className = 'card';
+    cardElement.setAttribute('data-card', cards[i]);
+    cardElement.addEventListener('click', isTwoCards);
     board.appendChild(cardElement);
   }
 }
-createCards();
 
+function isTwoCards() {
+	cardsInPlay.push(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'queen') {
+		this.innerHTML = "<img src = 'queenPic.jpg'>";
+	} else {
+		this.innerHTML = "<img src = 'kingPic.jpg'>";
+	}
 
+	if (cardsInPlay.length === 2) {
+		isMatch(cardsInPlay);
+		cardsInPlay = [];
+	}
+}
+
+function isMatch(cards) {
+	if (cards[0] === cards[1]) {
+		alert("You found a match!");	
+	} else {
+	  alert("Sorry, try again.");
+  }
+}
+function clearGuess(){   
+        document.getElementById('game-board').innerHTML = '';
+        createBoard();
+    }
+
+createBoard();
