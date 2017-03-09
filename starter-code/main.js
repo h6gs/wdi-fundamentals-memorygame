da3c44627b28b57f3ad95291471baafb70bd9a38
Harry Stirling <harrystirling@sh.local>
2017-03-09 10:06:04 +0000

    unit 10 assignment updates

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,10 +1,24 @@
-var cardOne = 'queen';
+/*var cardOne = 'queen';
 var cardTwo = 'queen';
 var cardThree = 'king';
 var cardFour = 'king';
 
-if (cardTwo === cardFour) {
+if (cardOne === cardTwo || cardThree === cardFour) {
 	alert('You found a match!');	
 } else {
   alert('Sorry, try again.');
 }
+*/
+var cards = ['queen', 'queen', 'king', 'king'];
+var board = document.getElementById('game-board');
+
+function createCards() {
+  for (var i=0; i < cards.length; i++) {
+    var cardElement = document.createElement('div')
+    cardElement.className = 'card';
+    board.appendChild(cardElement);
+  }
+}
+createCards();
+
+
